5f0892399871fc49cfaeb740e9561011099a46b7
Kacur Jan <xkacur04@merlin.fit.vutbr.cz>
2022-02-25 12:01:28 +0100

    Added declarations to header file

--- stonks.h
+++ stonks.h
@@ -2,3 +2,11 @@
 #include <stdio.h>
 #define NUM_INSTRUMENTS 5
 #define INIT_BALANCE 1000
+
+struct instrument;
+struct state;
+struct state* initState();
+void updateInstrument(struct state*, int, float);
+void sellInstrument(struct state*, int, int);
+void buyInstrument(struct state*, int, int);
+float portfolioValue(struct state*);
