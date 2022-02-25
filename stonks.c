8abfdf514f675a6f82e7ed698c0e3ccc6d28ad50
Kacur Jan <xkacur04@merlin.fit.vutbr.cz>
2022-02-25 11:11:24 +0100

    Function for updating instrument state

--- stonks.c
+++ stonks.c
@@ -1,18 +1,52 @@
-#include <stonks.h>
+#include "stonks.h"
 
 struct instrument
 {
 	int len;
-	int data[50000];
+	float data[50000];
+	int held;
 };
 
 struct state
 {
-	int balance;
-	struct instrument data[5];
+	float balance;
+	struct instrument* instruments[NUM_INSTRUMENTS];
 };
 
 int getPrice()
 {
+	static int x = 0;
+	return (x++)%3;
+}
+
+struct state* initState()
+{
+	struct state* state = malloc(sizeof(struct state));
+	state->balance = INIT_BALANCE;
+	int i;
+	for(i = 0; i < NUM_INSTRUMENTS; i++)
+	{
+		struct instrument* instrument = malloc(sizeof(struct instrument));
+		instrument->len = 0;
+		instrument->held = 0;
+		state->instruments[i] = instrument;
+	}
+	
+	return state;
+}
+
+struct state* updateInstrument(struct state* state, int index, float price)
+{
+	int currentLen = state->instruments[index]->len;
+	state->instruments[index]->data[currentLen] = price;
+	state->instruments[index]->len++;
+	return state;
+}
+
+int main()
+{
+	struct state* state = initState();
+	updateInstrument(state, 3, 52.56);
+	printf("%f\n", state->instruments[3]->data[0]);
 	return 0;
 }
---
+++ stonks.h
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+#include <stdio.h>
+#define NUM_INSTRUMENTS 5
+#define INIT_BALANCE 1000
