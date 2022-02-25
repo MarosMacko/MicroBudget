d4094dd4085c00bc63113058738e029b1e09f34a
Klement Matej (211229) <xkleme14@vutbr.cz>
2022-02-25 14:01:03 +0100

    changes

---
+++ rng.c
@@ -0,0 +1,12 @@
+#include "math.h" // for RAND, and rand
+
+#define RAND_MAX 100
+
+double sampleNormal() {
+    double u = ((double) rand() / (RAND_MAX)) * 2 - 1;
+    double v = ((double) rand() / (RAND_MAX)) * 2 - 1;
+    double r = u * u + v * v;
+    if (r == 0 || r > 1) return sampleNormal();
+    double c = sqrt(-2 * log(r) / r);
+    return u * c;
+}
